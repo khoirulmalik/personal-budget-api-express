1b6dab3d0cfd740eb00efb2f466ca020ab0fbe3f
malik <khoirulmalik03@gmail.com>
2024-09-22 18:52:06 +0700

    add /envelope dan /envelope/:id

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const app = express();
+const myEnvelope = require("./myEnvelope");
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("test");
+app.get("/envelope", (req, res) => {
+  res.send(myEnvelope);
+});
+
+app.get("/envelope/:id", (req, res) => {
+  const getById = parseInt(req.params.id);
+  const envelope = myEnvelope.find((env) => env.id === getById);
+
+  console.log(envelope); // Log the envelope
+
+  if (envelope) {
+    res.send(envelope);
+  } else {
+    res.status(404).send("Envelope not found");
+  }
 });
 
 app.listen(PORT, () => {
--- utils.js
+++ utils.js
@@ -1,3 +1,19 @@
 const myEnvelope = require("./myEnvelope");
 
 console.log(myEnvelope[0].id);
+
+// const findById = (arr, id) => {
+//   const envelopeExits = (arr, newEnvelope) => {
+//     arr.some((envelope) => envelope.id === newEnvelope.id);
+//   };
+
+//   if (envelopeExits) {
+//     console.log(`id already exits`);
+//   } else {
+//     arr.push[newEnvelope];
+//   }
+
+//   console.log(newEnvelope);
+// };
+
+// findById(myEnvelope, 2);
